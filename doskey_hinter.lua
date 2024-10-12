fee6672f48332822d0502ffb2c4a2239c6ceddd6
Chris Antos <sparrowhawk996@gmail.com>
2024-10-12 12:18:02 -0700

    Add some input hinters for Clink v1.7.0.
    doskey_hinter shows what doskey macros expand to.
    highlight_envvars shows when environment variables expand to.

---
+++ doskey_hinter.lua
@@ -0,0 +1,61 @@
+-- Shows an optional preview of a doskey alias entered in the input line.
+--
+-- By default the preview is disabled.
+--
+-- Run `clink set doskey.show_preview true` to enable the preview.
+--
+-- Requires Clink v1.7.0 or higher.
+
+if not clink.hinter then
+    log.info("doskey_hinter requires a newer version of Clink; please upgrade.")
+    return
+end
+
+settings.add("doskey.show_preview", true, "Show preview of doskey alias in the input line",
+             "When both the comment_row.show_hints and doskey.show_preview settings are\n"..
+             "enabled and the cursor is on a doskey alias, then a preview is shown in the\n"..
+             "comment row.")
+
+local enabled
+local function init_enabled()
+    enabled = settings.get("doskey.show_preview")
+end
+clink.onbeginedit(init_enabled)
+
+local function expand_ctrl(text)
+    local s = ""
+    if text then
+        local i = 1
+        while i <= #text do
+            local next = text:find("[\x01-\x1f]")
+            if not next then
+                s = s..text:sub(i)
+                break
+            end
+            s = s.."^"..string.char(64 + text:byte(next))
+            i = next + 1
+        end
+    end
+    return s
+end
+
+local hinter = clink.hinter(1)
+function hinter:gethint(line_state) -- luacheck: no self
+    if not enabled then
+        return
+    end
+
+    local cursorpos = line_state:getcursor() or 1
+    local commandwordindex = line_state:getcommandwordindex()
+
+    local info = line_state:getwordinfo(commandwordindex)
+    if info and info.alias and info.offset <= cursorpos and cursorpos <= info.offset + info.length then
+        local alias = line_state:getword(commandwordindex) or ""
+        if alias ~= "" then
+            local command = os.getalias(alias) or ""
+            if command ~= "" then
+                return "Alias "..expand_ctrl(alias).." = "..expand_ctrl(os.getalias(alias)), info.offset
+            end
+        end
+    end
+end
--- highlight_envvars.lua
+++ highlight_envvars.lua
@@ -38,3 +38,59 @@ function ev:classify(commands) -- luacheck: no unused
         end
     end
 end
+
+if clink.hinter then
+
+    settings.add("highlight_envvars.show_preview", true, "Show preview of environment variable at cursor",
+                 "When both the comment_row.show_hints and highlight_envvars.show_preview\n"..
+                 "settings are enabled and the cursor is on a doskey alias, then a preview is\n"..
+                 "shown in the comment row.")
+
+    local enabled
+    local function init_enabled()
+        enabled = settings.get("highlight_envvars.show_preview")
+    end
+    clink.onbeginedit(init_enabled)
+
+    local eh = clink.hinter(1)
+    function eh:gethint(line_state) -- luacheck: no self
+        if not enabled then
+            return
+        end
+
+        local hint
+        local pos
+
+        local line = line_state:getline()
+        local in_out = false
+        local index = 0
+        local cursorpos = line_state:getcursor() or 1
+
+        while index <= cursorpos do
+            local next = line:find("%", index + 1, true--[[plain]])
+            if not next then
+                break
+            end
+            in_out = not in_out
+            if in_out then
+                index = next
+            else
+                local name = line:sub(index + 1, next - 1)
+                local value = os.getenv(name)
+                if value then
+                    if index <= cursorpos and cursorpos <= next + 1 then
+                        hint = string.format("Envvar %s = %s", name, tostring(value))
+                        pos = index
+                    end
+                    index = next
+                else
+                    in_out = true
+                    index = next
+                end
+            end
+        end
+
+        return hint, pos
+    end
+
+end
