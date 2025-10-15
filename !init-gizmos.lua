f467496ad3323ae8550e7edd8cbbd54c95725b4a
Chris Antos <sparrowhawk996@gmail.com>
2025-10-14 23:23:47 -0700

    Refactor matchicons.lua into a Lua module.
    This makes it possible for scripts to cooperatively ensure that the
    matchicons_module is loaded early enough to affect all other scripts.

--- !init-gizmos.lua
+++ !init-gizmos.lua
@@ -1,3 +1,12 @@
 -- The line below extends package.path with modules
 -- directory to allow to require them
 package.path = debug.getinfo(1, "S").source:match[[^@?(.*[\/])[^\/]-$]] .."modules/?.lua;".. package.path
+
+-- Load matchicons_module.lua early, if present, to ensure all clink-gizmos
+-- scripts gain match icons when appropriate.
+local function try_require(module)
+    local r
+    pcall(function() r = require(module) end)
+    return r
+end
+try_require("matchicons_module")
