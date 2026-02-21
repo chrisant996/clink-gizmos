31bbf16cb80041c53f9c83f31f3ca3e943308178
Chris Antos <sparrowhawk996@gmail.com>
2026-02-20 21:53:50 -0800

    Early modifications to the original script.
    https://github.com/chrisant996/clink-gizmos/issues/20#issuecomment-3932229680

--- fzf_rg.lua
+++ fzf_rg.lua
@@ -1,5 +1,6 @@
 --------------------------------------------------------------------------------
--- Provides a command for showing a list of files matching a search pattern and opening the files your editor
+-- Provides a command for showing a list of files matching a search pattern and
+-- opening the files your editor.
 --
 -- KEY BINDING:
 --
@@ -16,13 +17,22 @@
 ]]
 --------------------------------------------------------------------------------
 
-
 local function add_desc(macro, desc)
     if rl.describemacro then
         rl.describemacro(macro, desc)
     end
 end
 
+local function need_quote(word)
+    return word and word:find("[ &()[%]{}^=;!%%'+,`~]") and true
+end
+
+local function maybe_quote(word)
+    if need_quote(word) then
+        word = '"'..word..'"'
+    end
+    return word
+end
 
 add_desc("luafunc:ripgrep_popup", "Show a FZF filtered view with files matching search term")
 
@@ -35,7 +45,7 @@ function ripgrep_popup(rl_buffer, line_state) -- luacheck: no unused
     -- --disabled: Tells fzf not to filter results itself
     -- --bind "change:reload...": Runs rg every time the input changes
     -- {q}: Is the placeholder for the fzf input string
-    
+
     -- If the line is empty, we'll let ripgrep prompt for input inside fzf
     -- Otherwise, we use the current line as the initial ripgrep query
     local args = {
@@ -54,68 +64,105 @@ function ripgrep_popup(rl_buffer, line_state) -- luacheck: no unused
     local handle = io.popen(fzf_cmd)
     local result = handle:read("*a")
     handle:close()
-    
+
+    -- Redraw the prompt and input line
+    rl_buffer:refreshline()
+
     -- If the user cancelled fzf, result will be empty
-    if result ~= "" then
-        -- fzf output format: file:line:column:text
-        local file, line = result:match("([^:]+):([^:]+):")
-        
-        if file and line then
-            -- Prepare the command to open the editor
-            -- Uses EDITOR environment variable, defaults to 'vim'
-            local editor = os.getenv("EDITOR") or "C:\\Program Files\\Sublime Text\\subl.exe"
-            local final_cmd = string.format('"%s" "%s:%s"', editor, file, line)
+    if not result or result == "" then
+        return
+    end
 
-            -- 1. Modern IDEs & Cross-platform (VS Code, Zed, Sublime)
-            if editor:find("code") or editor:find("zed") then
-                final_cmd = string.format('"%s" --goto "%s:%s"', editor, file, line)
+    -- fzf output format: file:line:column:text
+    local file, line = result:match("([^:]+):([^:]+):")
+    if not file or not line then
+        return
+    end
 
-            elseif editor:find("subl") or editor:find("emacs") or editor:find("hx") or editor:find("micro") then
-                final_cmd = string.format('"%s" "%s:%s"', editor, file, line)
+    -- Prepare the command to open the editor
+    -- Uses EDITOR environment variable, defaults to 'vim'
+    local editor = os.getenv("EDITOR") or path.join(os.getenv("windir"), "System32\\notepad.exe")
 
-            -- 2. Notepad++ (The Windows favorite)
-            elseif editor:find("notepad++") or editor:find("npp") then
-                final_cmd = string.format('"%s" -n%s "%s"', editor, line, file)
-
-            -- 3. UltraEdit (uedit64 / uedit32)
-            elseif editor:find("uedit") then
-                -- UltraEdit uses file.txt/line syntax
-                final_cmd = string.format('"%s" "%s/%s"', editor, file, line)
-
-            -- 4. EditPlus
-            elseif editor:find("editplus") then
-                final_cmd = string.format('"%s" -cursor %s:1 "%s"', editor, line, file)
-
-            -- 5. PSPad
-            elseif editor:find("pspad") then
-                final_cmd = string.format('"%s" /%s "%s"', editor, line, file)
-
-            -- 6. JetBrains (IntelliJ, WebStorm, etc.)
-            elseif editor:find("idea") or editor:find("storm") or editor:find("rider") then
-                final_cmd = string.format('"%s" --line %s "%s"', editor, line, file)
-
-            -- 7. Standard Windows Notepad (Fallback)
-            elseif editor == "notepad" or editor == "notepad.exe" then
-                -- Notepad doesn't support line flags via CLI.
-                -- We open it, then Clink can't automate the jump easily, 
-                -- so we just open the file.
-                final_cmd = string.format('notepad "%s"', file)
-            
-            -- 8. CLI Editors (Vim, Nano)
-            else
-                final_cmd = string.format('"%s" +"%s" "%s"', editor, line, file)
+    local haystack = editor:lower()
+    local quotable
+    if os.isfile(haystack) then
+        quotable = true
+        haystack = path.getname(haystack)
+    end
+    local function test(...)
+        for _, pattern in ipairs({...}) do
+            for _, suffix in ipairs({'', '%.exe', '%.cmd', '%.bat', '%.pl', '%.py'}) do
+                if haystack == pattern or
+                        haystack == pattern..suffix:sub(2) or
+                        haystack:find('^"?'..pattern..suffix..'[ "]') or
+                        haystack:find('[ "/\\]'..pattern..suffix..'[ "]') then
+                    return true
+                end
             end
-            final_cmd = final_cmd
-
-            -- if executable has spaces just running final_cmd doesn't seem to work
-            os.execute('cmd /s /c "' .. final_cmd .. '"')
-            
-            rl_buffer:beginundogroup()
-            rl_buffer:setcursor(1)
-            rl_buffer:remove(1, rl_buffer:getlength() + 1)  -- remove what the user might have started with
-            rl_buffer:endundogroup()
         end
     end
+
+    local final_cmd = quotable and maybe_quote(editor) or editor
+    local function append_cmd(fmt, ...)
+        final_cmd = final_cmd..' '..string.format(fmt, ...)
+    end
+
+    -- 1. Modern IDEs & Cross-platform (VS Code, Zed, Sublime)
+    if test("code") or
+            test("zed") then
+        append_cmd('--goto "%s:%s"', file, line)
+
+    elseif test("subl") or
+            test("emacs") or
+            test("hx") or
+            test("micro") then
+        append_cmd('"%s:%s"', file, line)
+
+    -- 2. Notepad++
+    elseif test("notepad++") or
+            test("npp") then
+        append_cmd('-n%s "%s"', line, file)
+
+    -- 3. UltraEdit (uedit64 / uedit32)
+    elseif test("uedit") then
+        -- UltraEdit uses file.txt/line syntax
+        append_cmd('"%s/%s"', file, line)
+
+    -- 4. EditPlus
+    elseif test("editplus") then
+        append_cmd('-cursor %s:1 "%s"', line, file)
+
+    -- 5. PSPad
+    elseif test("pspad") then
+        append_cmd('/%s "%s"', line, file)
+
+    -- 6. JetBrains (IntelliJ, WebStorm, etc.)
+    elseif test("idea") or
+            test("storm") or
+            test("rider") then
+        append_cmd('--line %s "%s"', line, file)
+
+    -- 7. CLI Editors (Vim, Nano, Edit)
+    elseif test("vim") or
+            test("nano") or
+            test("edit") then
+        append_cmd('+"%s" "%s"', line, file)
+
+    else
+        append_cmd('"%s"', file)
+    end
+
+    -- If the command line to execute begins with a quote and contains
+    -- more than one pair of quotes, then special quote handling is
+    -- necessary.
+    if final_cmd:find('^%s*"') then
+        os.execute('cmd /s /c "'..final_cmd..'"')
+    else
+        os.execute(final_cmd)
+    end
+
+    -- Discard what the user might have started with
+    rl.invokecommand("clink-reset-line")
 end
 
 if rl.getbinding then
